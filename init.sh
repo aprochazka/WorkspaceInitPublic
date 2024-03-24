22549ef6f77dc30aac79ea1959e803e5910aad65
Adam Prochazka <a@a.localdomain>
2024-03-24 09:29:47 +0100

    progress in init script

--- init.sh
+++ init.sh
@@ -14,9 +14,10 @@ echo
 echo "Downloading gh cli...."
 echo
 
-sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list \
 && sudo apt update \
 && sudo apt install gh -y
 
@@ -35,6 +36,8 @@ echo "Removing temporaty token file"
 rm -rf token
 
 echo "Clonning Init workspace repo"
-gh repo clone aprochazka/WorkspaceInit
+gh repo clone aprochazka/WorkspaceInit ../WorkspaceInit
 
+echo "Running Init Workspace Script.."
+bash ../WorkspaceInit/initPrivate.sh
 
